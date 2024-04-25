94e7fb2aaa21ec6fa035996262c5f10b503f4a8c
DeivisonSantos <deivisonddj3@gmail.com>
2024-04-25 01:44:58 -0300

    added color, does it really work? Hire me to know ;)

--- EDF/src/enums/TaskStats.java
+++ EDF/src/enums/TaskStats.java
@@ -4,7 +4,7 @@ public enum TaskStats {
     CREATED (ChangeColor.YELLOW.getColor() + "Created" + ChangeColor.DEFAULT.getColor()),
     READY (ChangeColor.YELLOW.getColor() + "Ready" + ChangeColor.DEFAULT.getColor()),
     START_RUNNING (ChangeColor.YELLOW.getColor() + "Starts Running" + ChangeColor.DEFAULT.getColor()),
-    RUNNING (ChangeColor.YELLOW.getColor() + "Running" + ChangeColor.DEFAULT.getColor()),
+    RUNNING ("Running"),
     PREEMPTED (ChangeColor.RED.getColor() + "Pre-empted" + ChangeColor.DEFAULT.getColor()),
     COMPLETED (ChangeColor.GREEN.getColor() + "Completed" + ChangeColor.DEFAULT.getColor())
     ;
